175c7e8fb50a03c405b4711691abbc73f6c9cb1f
Ákos Kiss <ak@coram.pub>
2022-11-13 20:03:57 +0100

    Implement some error handling for static tables

--- duckdb-static-table.lisp
+++ duckdb-static-table.lisp
@@ -29,29 +29,40 @@
   (foreign-free ptr))
 
 (defcallback static-table-bind :void ((info duckdb-bind-info))
-  (let* ((bind-data (foreign-alloc '(:struct static-table-bind-data-struct))))
-    (with-duckdb-value (table-name-param (duckdb-bind-get-parameter info 0))
-      (let* ((table-name (duckdb-get-varchar table-name-param)))
-        (loop :for (column-name . values) :in (get-table table-name)
-              :for duckdb-type := (static-vector-type values)
-              :do (with-logical-type (type duckdb-type)
-                    (duckdb-bind-add-result-column info column-name type)))
-        (setf (foreign-slot-value bind-data
-                                  '(:struct static-table-bind-data-struct)
-                                  'table-name)
-              table-name)))
-    (duckdb-bind-set-bind-data info bind-data (callback free-ptr))))
+  (handler-case
+      (let* ((bind-data (foreign-alloc '(:struct static-table-bind-data-struct))))
+        (with-duckdb-value (table-name-param (duckdb-bind-get-parameter info 0))
+          (let* ((table-name (duckdb-get-varchar table-name-param))
+                 (table (get-table table-name)))
+            (unless table
+              (error (format nil "table ~s is undefined" table-name)))
+            (loop :for (column-name . values) :in table
+                  :for duckdb-type := (static-vector-type values)
+                  :do (with-logical-type (type duckdb-type)
+                        (duckdb-bind-add-result-column info column-name type)))
+            (setf (foreign-slot-value bind-data
+                                      '(:struct static-table-bind-data-struct)
+                                      'table-name)
+                  table-name)))
+        (duckdb-bind-set-bind-data info bind-data (callback free-ptr)))
+    (error (c)
+      (duckdb-bind-set-error info
+                             (format nil "static_table bind - ~a" c)))))
 
 (defcstruct static-table-init-data-struct
   (index :int64))
 
 (defcallback static-table-init :void ((info duckdb-bind-info))
-  (let ((init-data (foreign-alloc '(:struct static-table-init-data-struct))))
-    (setf (foreign-slot-value init-data
-                              '(:struct static-table-init-data-struct)
-                              'index)
-          0)
-    (duckdb-init-set-init-data info init-data (callback free-ptr))))
+  (handler-case
+      (let ((init-data (foreign-alloc '(:struct static-table-init-data-struct))))
+        (setf (foreign-slot-value init-data
+                                  '(:struct static-table-init-data-struct)
+                                  'index)
+              0)
+        (duckdb-init-set-init-data info init-data (callback free-ptr)))
+    (error (c)
+      (duckdb-init-set-error info
+                             (format nil "static_table init - ~a" c)))))
 
 (defmacro copy-static-vector ()
   `(ecase duckdb-type
@@ -70,24 +81,27 @@
 
 (defcallback static-table-function :void ((info duckdb-bind-info)
                                           (output duckdb-data-chunk))
-  (let* ((bind-data (duckdb-function-get-bind-data info))
-         (init-data (duckdb-function-get-init-data info))
-         (table-name (foreign-slot-value bind-data
-                                         '(:struct static-table-bind-data-struct)
-                                         'table-name)))
-
-    (with-foreign-slots ((index) init-data (:struct static-table-init-data-struct))
-      (let ((n (loop :for column :in (get-table table-name)
-                     :for values :of-type vector := (cdr column)
-                     :for column-index :from 0
-                     :for duckdb-type := (static-vector-type values)
-                     :for ffi-type := (get-ffi-type duckdb-type)
-                     :for vector := (duckdb-data-chunk-get-vector output column-index)
-                     :for data-length := (length values)
-                     :for data-ptr := (duckdb-vector-get-data vector)
-                     :maximize (copy-static-vector))))
-        (setf index (+ n index))
-        (duckdb-data-chunk-set-size output n)))))
+  (handler-case
+      (let* ((bind-data (duckdb-function-get-bind-data info))
+             (init-data (duckdb-function-get-init-data info))
+             (table-name (foreign-slot-value bind-data
+                                             '(:struct static-table-bind-data-struct)
+                                             'table-name)))
+        (with-foreign-slots ((index) init-data (:struct static-table-init-data-struct))
+          (let ((n (loop :for column :in (get-table table-name)
+                         :for values :of-type vector := (cdr column)
+                         :for column-index :from 0
+                         :for duckdb-type := (static-vector-type values)
+                         :for ffi-type := (get-ffi-type duckdb-type)
+                         :for vector := (duckdb-data-chunk-get-vector output column-index)
+                         :for data-length := (length values)
+                         :for data-ptr := (duckdb-vector-get-data vector)
+                         :maximize (copy-static-vector))))
+            (setf index (+ n index))
+            (duckdb-data-chunk-set-size output n))))
+    (error (c)
+      (duckdb-function-set-error info
+                                 (format nil "static_table function - ~a" c)))))
 
 (defun register-static-table-function (connection)
   (with-table-function (function)
